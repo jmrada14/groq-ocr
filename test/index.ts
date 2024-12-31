a65c322c7120d0d2475ffee2d90f47b65b48b390
jmrada14 <jmrada14@gmail.com>
2024-12-31 13:38:38 -0600

    update tests

--- test/index.ts
+++ test/index.ts
@@ -1,16 +1,17 @@
 import fs from "fs";
+import path from "path";
 import { GroqVisionModel, ocr } from "../src";
 import { config } from "dotenv";
 config();
 
 async function main() {
+  // Test OCR on a local image file to markdown
   let markdown = await ocr({
-    filePath: "./image.jpg",
+    filePath: path.join(__dirname, "../test/image.jpg"),
     apiKey: process.env.GROQ_API_KEY,
     model: GroqVisionModel.LLAMA_32_90B,
   });
-  // write the markdown to a file
-  fs.writeFileSync("output.md", markdown);
+  fs.writeFileSync(path.join(__dirname, "output_image_to_md.md"), markdown);
 }
 
 main();
